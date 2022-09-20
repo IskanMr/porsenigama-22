905e756bfa19b4f1caa71b17632e1eea28250372
HerviBest <hervipro@gmail.com>
2022-09-20 09:21:29 +0700

    dataCabangs data gabungan casen sama cabor.
    Data cabang olahraga belum fix, tinggal nambah dari aufa (data cabor baru). Data Casen belum fix

--- src/data/dataCabangs.js
+++ src/data/dataCabangs.js
@@ -171,12 +171,12 @@ const caborList = [
       {
         category: "Kelas A Putra (45-50 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasC_PSG22'
+        link: 'http://challonge.com/SilatPaKlsA_PSG22'
       },
       {
         category: "Kelas B Putra (50-55 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasC_PSG22' 
+        link: 'http://challonge.com/SilatPaKlsB_PSG22' 
       },
       {
         category: "Kelas C Putra (55-60 kg)",
@@ -191,64 +191,64 @@ const caborList = [
       {
         category: "Kelas E Putra (65-70 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPaKlsE_PSG22'
       },
       {
         category: "Kelas F Putra (70-75 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPaKlsF_PSG22'
       },
       {
         category: "Kelas G Putra (75-80 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPaKlsG_PSG22'
       },
       {
         category: "Kelas H Putra (80-85 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPaKlsH_PSG22'
       },
       {
         category: "Kelas I Putra (85-90 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPaKlsI_PSG22'
       },
       {
         category: "Kelas A Putri (45-50 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPiKlsA_PSG22'
       },
       {
         category: "Kelas B Putri (50-55 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22' 
+        link: 'http://challonge.com/SilatPiKlsB_PSG22' 
       },
       {
         category: "Kelas C Putri (55-60 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPiKlsC_PSG22'
       },
       {
         category: "Kelas D Putri (60-65 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPiKlsD_PSG22'
       },
       {
         category: "Kelas E Putri (65-70 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPiKlsE_PSG22'
       },
       {
         category: "Kelas F Putri (70-75 kg)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/SilatPiKlsF_PSG22'
       },
     ],
   },
   {
     title: "Taekwondo",
     description:
-      "Penjelasan dikit po ya Penjelasan dikit po ya Penjelasan dikit po ya",
+      "Taekwondo merupakan olahraga bela diri yang berkembang pada 37 Masehi pada dinasti Kogooryo di Korea. Bela diri Taekwondo dikenal sebagai bela diri andalan yang digunakan para Ksatria. Di Indonesia sendiri Tae Kwon Do berkembang sekitar tahun 1975. Sebagai bela diri, Tae Kwon Do memiliki filosofi yang tertanam di dalamnya berupa budi pekerti, kejujuran, ketabahan, penguasaan diri, dan semangat pantang menyerah.",
     category: [
       "Putra Under 54 kg",
       "Putra Under 58 kg",
@@ -262,93 +262,95 @@ const caborList = [
       "Putri Under 57 kg",
       "Putri Under 62 kg",
       "Putri Over 62 kg",
-      "Putra A",
-      "Putra B",
-      "Putri A",
-      "Putri B",
+      "Putra A (Sabuk Merah-Hitam)",
+      "Putra B (Sabuk Putih-Biru)",
+      "Putri A (Sabuk Merah-Hitam)",
+      "Putri B (Sabuk Putih-Biru)",
 
     ],
     categoryData: [
       {
         category: "Putra Under 54 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatSeniPutraPSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU54PSG2022'
       },
       {
         category: "Putra Under 58 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatSeniPutriPSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU58PSG2022'
+      },
+      {
+        category: "Putra Under 63 kg",
+        iframe: true,
+        link: 'http://challonge.com/TaekwondoKyorugiU63PSG2022'
       },
       {
         category: "Putra Under 68 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasC_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU68PSG2022' 
       },
       {
         category: "Putra Under 74 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasC_PSG22' 
+        link: 'http://challonge.com/TaekwondoKyorugiU74PSG2022'
       },
       {
-        category: "Kelas C Putra (55-60 kg)",
+        category: "Putra Over 80 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasC_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiO80PSG2022'
       },
       {
-        category: "Kelas D Putra (60-65 kg)",
+        category: "Putri Under 46 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU46PSG2022'
       },
       {
-        category: "Kelas E Putra (65-70 kg)",
+        category: "Putri Under 49 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU49PSG2022'
       },
       {
-        category: "Kelas F Putra (70-75 kg)",
+        category: "Putri Under 53 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU53PSG2022'
       },
       {
-        category: "Kelas G Putra (75-80 kg)",
+        category: "Putri Under 57 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU57PSG2022'
       },
       {
-        category: "Kelas H Putra (80-85 kg)",
+        category: "Putri Under 62 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiU62PSG2022'
       },
       {
-        category: "Kelas I Putra (85-90 kg)",
+        category: "Putri Over 62 kg",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoKyorugiO62PSG2022'
       },
       {
-        category: "Kelas A Putri (45-50 kg)",
+        category: "Putra A (Sabuk Merah-Hitam)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoPoomsaePutraAPSG2022' 
       },
       {
-        category: "Kelas B Putri (50-55 kg)",
+        category: "Putra B (Sabuk Putih-Biru)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22' 
+        link: 'http://challonge.com/TaekwondoPoomsaePutraBPSG2022'
       },
       {
-        category: "Kelas C Putri (55-60 kg)",
+        category: "Putri A (Sabuk Merah-Hitam)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoPoomsaePutriAPSG2022' 
       },
       {
-        category: "Kelas D Putri (60-65 kg)",
+        category: "Putri B (Sabuk Putih-Biru)",
         iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
+        link: 'http://challonge.com/TaekwondoPoomsaePutriBPSG2022'
       },
-      {
-        category: "Kelas E Putri (65-70 kg)",
-        iframe: true,
-        link: 'http://challonge.com/SilatPaKelasD_PSG22'
-      },
+      
+
     ],
   },
   {
@@ -356,7 +358,7 @@ const caborList = [
     description:
       "Penjelasan dikit po ya Penjelasan dikit po ya Penjelasan dikit po ya",
     category: [
-      "Beregu Campuran",
+
       "Tunggal Putra",
       "Tunggal Putri",
       "Ganda Putra",
@@ -365,185 +367,208 @@ const caborList = [
     ],
     categoryData: [
       {
-        category: "Beregu Campuran",
-        link: 'test'
-      },
-      {
         category: "Tunggal Putra",
-        link: 'test'
+        link: 'http://challonge.com/BulutangkisTunggalPaPSG22'
       },
       {
         category: "Tunggal Putri",
-        link: 'test'
+        link: 'http://challonge.com/BulutangkisTunggalPiPSG22'
       },
       {
         category: "Ganda Putra",
-        link: 'test' 
+        link: 'http://challonge.com/BulutangkisGandaPaPSG22'
       },
       {
         category: "Ganda Putri",
-        link: 'test'
+        link: 'http://challonge.com/BulutangkisGandaPiPSG22'
       },
       {
         category: "Ganda Campuran",
-        link: 'test'
+        link: 'http://challonge.com/BulutangkisGandaCampuranPSG22'
       },
     ],
   },
   {
     title: "Bridge",
     description:
-      "Penjelasan dikit po ya Penjelasan dikit po ya Penjelasan dikit po ya",
+      "Bridge merupakan salah satu cabang olahraga bermain kartu yang membutuhkan kejelian dalam melihat peluang maupun penyusunan strategi yang bisa mengasah otak para pemainnya. Bridge bernaung di bawah World Bridge Federation (WBF) yang berdiri sejak tahun 1958. ",
     category: [
-      "Beregu Campuran",
-      "Tunggal Putra",
-      "Tunggal Putri",
-      "Ganda Putra",
-      "Ganda Putri",
-      "Ganda Campuran",
+      "Bridge Individu",
+      "Bridge Pasangan",
+      "Bridge Beregu",
+
     ],
     categoryData: [
       {
-        category: "Beregu Campuran",
-        link: 'test'
+        category: "Bridge Individu",
+        link: 'http://challonge.com/BridgeIndividuPSG22'
       },
       {
-        category: "Tunggal Putra",
-        link: 'test'
+        category: "Bridge Pasangan",
+        link: 'http://challonge.com/BridgePasanganPSG22'
       },
       {
-        category: "Tunggal Putri",
-        link: 'test'
+        category: "Bridge Beregu",
+        link: 'http://challonge.com/BridgeBereguPSG22'
       },
-      {
-        category: "Ganda Putra",
-        link: 'test' 
-      },
-      {
-        category: "Ganda Putri",
-        link: 'test'
-      },
-      {
-        category: "Ganda Campuran",
-        link: 'test'
-      },
+
     ],
   },
   {
     title: "Catur",
     description:
-      "Penjelasan dikit po ya Penjelasan dikit po ya Penjelasan dikit po ya",
+      "Catur adalah permainan papan strategi dua orang yang dimainkan pada sebuah papan kotak-kotak yang terdiri dari 64 kotak, yang disusun dalam petak 8×8, yang terbagi sama rata dalam kelompok warna putih dan hitam.",
     category: [
-      "Beregu Campuran",
-      "Tunggal Putra",
-      "Tunggal Putri",
-      "Ganda Putra",
-      "Ganda Putri",
-      "Ganda Campuran",
+      "Catur Cepat Campuran",
+      "Catur Cepat Putra",
+      "Catur Cepat Putri",
+      "Catur Kilat Umum",
+
     ],
     categoryData: [
       {
-        category: "Beregu Campuran",
-        link: 'test'
+        category: "Catur Cepat Campuran",
+        link: 'http://challonge.com/CaturCepatCampuranPSG22'
       },
       {
-        category: "Tunggal Putra",
-        link: 'test'
+        category: "Catur Cepat Putra",
+        link: 'http://challonge.com/CaturCepatPutraPSG22'
       },
       {
-        category: "Tunggal Putri",
-        link: 'test'
+        category: "Catur Cepat Putri",
+        link: 'http://challonge.com/CaturCepatPutriPSG22'
       },
       {
-        category: "Ganda Putra",
-        link: 'test' 
-      },
-      {
-        category: "Ganda Putri",
-        link: 'test'
-      },
-      {
-        category: "Ganda Campuran",
-        link: 'test'
-      },
+        category: "Catur Kilat Umum",
+        link: 'http://challonge.com/CaturKilatUmumPSG22'
+      }
     ],
   },
   {
     title: "E-Sport",
     description:
-      "Esports adalah cabang olahraga modern yang menggunakan peralatan elektronik sebagai media dalam permainannya. Sebagian cabang games dalam Esports biasanya menggunakan PC/Komputer, Smartphone/Mobile, dan beberapa jenis konsol seperti Nintendo, XBOX, maupun PlayStation. Cabang olahraga ini mengandalkan otak untuk menghasilkan berpikir cepat untuk meraih kemenangan dalam kompetisi Esports.",
+      "Esports adalah cabang olahraga modern yang menggunakan peralatan elektronik sebagai media dalam permainannya. Cabang olahraga ini mengandalkan otak untuk berpikir cepat karena diperlukan strategi yang matang untuk meraih poin dan memenangkan games. Dalam Porsenigama 2022 kali ini, cabang games dalam Esports dimainkan dengan menggunakan PC/Komputer dan Smartphone/Mobile sebagai medianya.",
     category: [
       "Valorant",
       "Dota 2",
       "Mobile Legends",
-      "Ganda Putra",
-      "Ganda Putri",
-      "Ganda Campuran",
+      "PUBG Mobile",
+      "Magic Chess",
     ],
     categoryData: [
       {
         category: "Dota 2",
-        link: 'test'
+        link: 'http://challonge.com/ValorantPSG22'
       },
       {
-        category: "Tunggal Putra",
-        link: 'test'
+        category: "Valorant",
+        link: 'http://challonge.com/Dota2PSG22'
       },
       {
-        category: "Tunggal Putri",
-        link: 'test'
+        category: "Mobile Legends",
+        link: 'http://challonge.com/MobileLegendsPSG22'
       },
       {
-        category: "Ganda Putra",
-        link: 'test' 
+        category: "PUBG Mobile",
+        link: 'http://challonge.com/PUBGM_PSG22'
       },
       {
-        category: "Ganda Putri",
-        link: 'test'
+        category: "Magic Chess",
+        link: 'http://challonge.com/MagicChessPSG22'
       },
-      {
-        category: "Ganda Campuran",
-        link: 'test'
-      },
+
     ],
   },
   {
     title: "Judo",
     description:
-      "Penjelasan dikit po ya Penjelasan dikit po ya Penjelasan dikit po ya",
+      "Judo adalah olahraga bela diri yang menitik beratkan pada bantingan dan bukan pukulan maupun tendangan.",
     category: [
-      "Beregu Campuran",
-      "Tunggal Putra",
-      "Tunggal Putri",
-      "Ganda Putra",
-      "Ganda Putri",
-      "Ganda Campuran",
+      "Judo Kelas +100 kg Putra",
+      "Judo Kelas -100 kg Putra",
+      "Judo Kelas -73 kg Putra",
+      "Judo Kelas -60 kg Putra",
+      "Judo Kelas -90 kg Putra",
+      "Judo Kelas -81 kg Putra",
+      "Judo Kelas -78 kg Putri",
+      "Judo Kelas +78 kg Putri",
+      "Judo Kelas -66 kg Putra",
+      "Judo Kelas -55 kg Putra",
+      "Judo Kelas -63 kg Putri",
+      "Judo Kelas -70 kg Putri",
+      "Judo Kelas -57 kg Putri",
+      "Judo Kelas -52 kg Putri",
+      "Judo Kelas -45 kg Putri",
+      "Judo Kelas -48 kg Putri",
     ],
     categoryData: [
       {
-        category: "Dota 2",
-        link: 'test'
+        category: "Judo Kelas +100 kg Putra",
+        link: 'http://challonge.com/JudoPlus100PutraPSG22'
       },
       {
-        category: "Tunggal Putra",
-        link: 'test'
+        category: "Judo Kelas -100 kg Putra",
+        link: 'http://challonge.com/JudoMin100PutraPSG22'
       },
       {
-        category: "Tunggal Putri",
-        link: 'test'
+        category: "Judo Kelas -73 kg Putra",
+        link: 'http://challonge.com/JudoMin73PutraPSG22'
       },
       {
-        category: "Ganda Putra",
-        link: 'test' 
+        category: "Judo Kelas -60 kg Putra",
+        link: 'http://challonge.com/JudoMin60PutraPSG22'
       },
       {
-        category: "Ganda Putri",
-        link: 'test'
+        category: "Judo Kelas -90 kg Putra",
+        link: 'http://challonge.com/JudoMin90PutraPSG22'
       },
       {
-        category: "Ganda Campuran",
-        link: 'test'
+        category: "Judo Kelas -81 kg Putra",
+        link: 'http://challonge.com/JudoMin81PutraPSG22'
       },
+      {
+        category: "Judo Kelas -78 kg Putri",
+        link: 'http://challonge.com/JudoMin78PutriPSG22'
+      },
+      {
+        category: "Judo Kelas +78 kg Putri",
+        link: 'http://challonge.com/JudoPlus78PutriPSG22'
+      },
+      {
+        category: "Judo Kelas -66 kg Putra",
+        link: 'http://challonge.com/JudoMin66PutraPSG22'
+      },
+      {
+        category: "Judo Kelas -55 kg Putra",
+        link: 'http://challonge.com/JudoMin55PutraPSG22'
+      },
+      {
+        category: "Judo Kelas -63 kg Putri",
+        link: 'http://challonge.com/JudoMin63PutriPSG22'
+      },
+      {
+        category: "Judo Kelas -70 kg Putri",
+        link: 'http://challonge.com/JudoMin70PutriPSG22'
+      },
+      {
+        category: "Judo Kelas -57 kg Putri",
+        link: 'http://challonge.com/JudoMin57PutriPSG22'
+      },
+      {
+        category: "Judo Kelas -52 kg Putri",
+        link: 'http://challonge.com/JudoMin52PutriPSG22'
+      },
+      {
+        category: "Judo Kelas -45 kg Putri",
+        link: 'http://challonge.com/JudoMin45PutriPSG22'
+      },
+      {
+        category: "Judo Kelas -48 kg Putri",
+        link: 'http://challonge.com/JudoMin48PutriPSG22'
+      },
+
+
+
     ],
   },
 
