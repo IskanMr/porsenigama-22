5eb1a4b827758e742f50ccf30975b8fd3af57a58
iskanmr <iskan.mustamir@mail.ugm.ac.id>
2022-09-27 00:09:16 +0700

    add(FAQ): add faq components; fix(FAQ): fix id of few questions

---
+++ src/Components/Faq/Faq.jsx
@@ -0,0 +1,63 @@
+import { Disclosure, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+
+export function FAQ({ item }) {
+  const disclosureContainerCSS = "my-2 bg-white rounded-lg items-center";
+  const disclosureButtonCSSClose =
+    "flex justify-between items-center w-full p-3 md:px-6 md:py-4 text-md md:text-[18px] font-gothammedium text-left focus:outline-none ";
+  const disclosureButtonCSSOpen =
+    "flex justify-between items-center w-full p-3 md:px-6 md:py-4 text-md md:text-[18px] font-gothammedium text-left focus:outline-none shadow-md";
+
+  const disclosurePanelCSS =
+    "p-3 md:px-6 md:py-4 pb-2 items-center text-sm md:text-[14px] font-gothambook";
+  const openPanelChevronIconCSS = "transform rotate-180";
+  const ChevronIconCSS = "w-5 h-5 md:w-7 md:h-7 text-cstmdarkergreen";
+
+  return (
+    <>
+      <Disclosure as="div" className={disclosureContainerCSS}>
+        {({ open }) => (
+          <>
+            <Disclosure.Button
+              className={
+                open ? disclosureButtonCSSOpen : disclosureButtonCSSClose
+              }
+            >
+              <div className="flex items-center">
+                <span className="text-xl md:text-2xl mr-3 text-oranges-default font-bold">
+                  Q
+                </span>
+                <span className="flex-grow">{item.question}</span>
+              </div>
+
+              <ChevronDownIcon
+                className={`${
+                  open ? openPanelChevronIconCSS : ""
+                } ${ChevronIconCSS}`}
+              />
+            </Disclosure.Button>
+            <Transition
+              enter="transition duration-100 ease-out"
+              enterFrom="transform scale-95 opacity-0"
+              enterTo="transform scale-100 opacity-100"
+              leave="transition duration-75 ease-out"
+              leaveFrom="transform scale-100 opacity-100"
+              leaveTo="transform scale-95 opacity-0"
+            >
+              {open && (
+                <Disclosure.Panel static className={disclosurePanelCSS}>
+                  <div className="flex items-center">
+                    <span className=" text-xl md:text-2xl mr-3 text-main-blue font-bold">
+                      A
+                    </span>
+                    <span className="flex-grow">{item.answer}</span>
+                  </div>
+                </Disclosure.Panel>
+              )}
+            </Transition>
+          </>
+        )}
+      </Disclosure>
+    </>
+  );
+}
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,10 +1,81 @@
+import { useState } from "react";
+
 import { HtmlHead } from "../Components/HtmlHead";
+import { FAQ } from "../Components/Faq/Faq";
+
+import { ReactComponent as Search } from "../assets/icons/search.svg";
+import Title from "../assets/images/faq/faqTitle.png";
+
+import Questions from "../resources/questions.json";
+import { openInNewTab } from "../utils/openInNewTab";
 
 const Faq = () => {
+  const [list, setList] = useState(Questions);
+  const onInputChange = (e) => {
+    setList(Questions.filter((a) => a.question.toLowerCase().includes(e)));
+  };
+  const onTabCategoryChange = (e) => {
+    setList(Questions.filter((a) => a.id === e));
+  };
+
+  const categories = [
+    { id: "umum", name: "Umum" },
+    { id: "cabang-olahraga", name: "Olahraga" },
+    { id: "tangkai-seni", name: "Seni" },
+  ];
+
   return (
     <>
       <HtmlHead title="Faq" decription="[insert page description]" />
-      <div className="min-h-screen">Faq</div>
+      <div className="min-h-screen bg-blue">
+        <div className="flex flex-col w-full justify-items-center items-center">
+          <span>
+            <img src={Title} className="" alt="title" />
+          </span>
+
+          <div className="flex w-full mt-6 justify-items-center place-content-center">
+            <label className="flex w-full md:w-2/4 p-3 bg-white rounded-xl items-center text-sm md:text-base">
+              <span>
+                <Search className="h-4 md:h-5" />
+              </span>
+              <input
+                className="flex flex-grow ml-1 md:ml-2 border-none outline-none"
+                placeholder="Ketik pertanyaanmu..."
+                type="text"
+                onChange={(e) => {
+                  onInputChange(e.target.value);
+                }}
+              />
+            </label>
+          </div>
+          <div className="flex w-full justify-items-center items-center">
+            {categories.map((category) => (
+              <button onClick={() => onTabCategoryChange(category.id)}>
+                {category.name}
+              </button>
+            ))}
+          </div>
+        </div>
+
+        <div className="flex justify-center mt-6">
+          <div className="flex flex-col w-100 md:w-3/4">
+            {list.map((item) => (
+              <FAQ item={item} />
+            ))}
+          </div>
+        </div>
+        <div className="flex justify-center mt-4 md:mt-8">
+          Pertanyaan kamu tidak ada di laman ini? Silahkan hubungi
+          <button
+            type="button"
+            onClick={() => openInNewTab("https://line.me/R/ti/p/@NRB2565A")}
+            className="ml-1 text-oranges-default hover:text-oranges-light transition ease-in-out duration-300"
+          >
+            {" "}
+            sini
+          </button>
+        </div>
+      </div>
     </>
   );
 };
