229a1b5056f08ed43c6158fa4e08e8831aa84e0b
iskanmr <iskan.mustamir@mail.ugm.ac.id>
2022-10-22 17:56:39 +0700

    feat(cabang): rundown button to newtab; version(build): new version

--- src/pages/Cabang.jsx
+++ src/pages/Cabang.jsx
@@ -1,7 +1,7 @@
 import { useState, useRef } from "react";
 import { caborList, casenList } from "../data/dataCabang";
 import { HtmlHead } from "../Components/HtmlHead";
-import styled from 'styled-components'
+import styled from "styled-components";
 import Logo from "../assets/images/cabang/Group 138.svg";
 import "../Components/Cabang/button.css";
 import TeksCabang from "../assets/images/cabang/Group 169.png";
@@ -22,42 +22,46 @@ const Cabang = () => {
   return (
     <>
       <HtmlHead title="Cabang" decription="[insert page description]" />
-      <div 
+      <div
         className="relative w-full h-full bg-blue py-14 lg:py-1 pb-20 sm:pb-40 md:pb-40 lg:pb-48"
         style={{
-          height: "100vh"
+          height: "100vh",
         }}
       >
-        <img 
-          src={RippedPaper} 
+        <img
+          src={RippedPaper}
           className="w-full absolute m-auto -bottom-14 425px:-bottom-20 sm:-bottom-20 md:-bottom-28 lg:-bottom-36 xl:-bottom-48 2xl:-bottom-60 pointer-events-none"
           alt="Robekan kertas"
         />
         <div className="w-3/4 sm:w-3/5 md:w-full flex flex-col-reverse justify-center md:grid grid-cols-1 md:grid-cols-2 justify-items-center content-center mx-auto">
           <div className="hidden w-full md:grid justify-items-center content-center pt-5 md:pt-16 px-10 z-10 pointer-events-none m-auto">
-            <img 
-              src={Cover} 
-              className="w-full lg:w-11/12 mb-8" 
-              style={{ width: "600px"}} 
+            <img
+              src={Cover}
+              className="w-full lg:w-11/12 mb-8"
+              style={{ width: "600px" }}
               alt="Orang yang sedang bermain basket dan menari"
             />
           </div>
           <div className="md:hidden w-full pt-14 pb-14 px-10 grid justify-items-center content-center z-10 pointer-events-none m-auto">
-            <img 
-              src={CoverMobile} 
-              className="w-full" 
-              style={{ width:"300px"}} 
+            <img
+              src={CoverMobile}
+              className="w-full"
+              style={{ width: "300px" }}
               alt="Orang yang sedang bermain basket dan menari"
             />
           </div>
           <div className="m-auto flex flex-col mr-6 lg:mr-14 z-10">
-            <img 
-              src={TeksCabang} 
-              className="pointer-events-none" 
-              alt="Cabang lomba" 
+            <img
+              src={TeksCabang}
+              className="pointer-events-none"
+              alt="Cabang lomba"
             />
             <div className="relative flex justify-center hover-trigger">
-              <a href="https://bit.ly/RilisJadwalCaborTasen" className="bg-orange text-white font-universalserif px-3 py-1.5 lg:px-4 lg:py-2 xl:px-5 text-xs 425px:text-base sm:text-lg xl:text-2xl 2xl:text-2xl rounded-xl xl:rounded-2xl transform duration-200 transition-300 hover:scale-110 cursor-pointer">
+              <a
+                href="https://bit.ly/RilisJadwalCaborTasen"
+                target="__blank"
+                className="bg-orange text-white font-universalserif px-3 py-1.5 lg:px-4 lg:py-2 xl:px-5 text-xs 425px:text-base sm:text-lg xl:text-2xl 2xl:text-2xl rounded-xl xl:rounded-2xl transform duration-200 transition-300 hover:scale-110 cursor-pointer"
+              >
                 Rundown
               </a>
               <span className="hover-target bg-purple px-4 py-2 text-xs 425px:text-sm xl:text-base text-center text-gray-100 rounded-xl absolute bottom-9 425px:bottom-12 md:bottom-12 lg:bottom-14 xl:bottom-14 mx-auto">
@@ -75,7 +79,11 @@ const Cabang = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-center bg-pattern-cabang" style={{backgroundSize: "cover"}} ref={content}>
+      <div
+        className="flex flex-col items-center justify-center bg-pattern-cabang"
+        style={{ backgroundSize: "cover" }}
+        ref={content}
+      >
         <div className="flex justify-center w-3/5 425px:w-1/2 sm:w-2/5 md:w-2/5 xl:w-1/3 pt-8 425px:pt-14 md:pt-20 my-8 sm:my-14 lg:my-14 xl:my-20 2xl:my-28 space-x-8 sm:space-x-14 lg:space-x-24">
           <button
             className="transform duration-200 transition-300 hover:scale-110"
@@ -122,6 +130,5 @@ const Cabang = () => {
     </>
   );
 };
-
 
 export { Cabang };
